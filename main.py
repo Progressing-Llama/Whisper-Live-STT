c86affaeff634f22696677d7bae0085aac09dd9b
Meezaan Ryklief <92272077+PCB-IT@users.noreply.github.com>
2024-12-20 15:54:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from live_stt import LiveSTT
+
+print("Initializing...")
+
+stt = LiveSTT()
+stt.calculate_recommended_settings(5)
+
+print("Running")
+
+stt.reset_ticker()
+
+while True:
+    stt.process()
